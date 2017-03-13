a908e7e38b24664c9f881c1097ccc7186beda06d
Sarah Williams <swilliams@atlasrfid.com>
2017-03-13 13:36:20 -0500

    Added code for day 1

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode_16_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShortPathCalc spc = new ShortPathCalc();
+            string path = File.ReadAllText("input.txt");
+            spc.followPath(path, true);
+            Console.Write(spc.distanceFromStart());
+            Console.ReadKey();
+        }
+    }
+}
