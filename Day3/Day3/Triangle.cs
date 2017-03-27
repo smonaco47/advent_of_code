4f5a66f1fe3484953de7c4561594415a7c49c99c
swilliams <swilliams@SW332017>
2017-03-27 08:43:04 -0500

    Completed Day 3

---
+++ Day3/Day3/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Triangle
+    {
+        public static bool IsValid(int side1, int side2, int side3)
+        {
+            var sides = new List<int>() {side1, side2, side3};
+            sides.Sort();
+            if ((sides[0] + sides[1]) > sides[2]) return true;
+            return false;
+        }
+    }
+}
